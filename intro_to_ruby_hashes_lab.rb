940c01c80dd5ead1f3c69fbc2d8704f6f564f400
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-08-28 19:36:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ def monopoly_with_third_tier
     :names => {
       :reading_railroad => {},
       :pennsylvania_railroad => {},
-      :third_railroad => {},
+      :b_and_o_railroad => {},
       :fourth_railroad => {}
     }
     }
