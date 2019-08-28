044806301fdaa3a8e9d851cef717d4383ff4f87d
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-08-28 19:45:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,9 +45,11 @@ def monopoly_with_fourth_tier
         "mortgage_value" => '$200'
       },
       :b_and_o_railroad => {
-        
+        "mortgage_value" => '$100'
       },
-      :shortline_railroad => {}
+      :shortline_railroad => {
+        
+      }
       }
     }
   }
